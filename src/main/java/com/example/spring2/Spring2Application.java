0efa25359e5ec5ded90991619b205a1d28597e55
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-04-11 22:28:33 +0900

    second

--- src/main/java/com/example/spring2/Spring2Application.java
+++ src/main/java/com/example/spring2/Spring2Application.java
@@ -3,6 +3,7 @@ package com.example.spring2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//main
 @SpringBootApplication
 public class Spring2Application {
 
